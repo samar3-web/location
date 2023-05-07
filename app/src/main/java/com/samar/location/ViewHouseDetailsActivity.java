58aee429c85377079a952d8bc3877d08317f6879
Toumi Mohamed Koussay <toumikoussay@hotmail.fr>
2023-05-07 23:39:31 +0100

    Adding map to ViewHouseDetailsActivity

--- app/src/main/java/com/samar/location/ViewHouseDetailsActivity.java
+++ app/src/main/java/com/samar/location/ViewHouseDetailsActivity.java
@@ -5,6 +5,10 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.PointF;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -22,6 +26,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
@@ -30,6 +35,7 @@ import androidx.viewpager.widget.ViewPager;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -37,11 +43,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.microsoft.maps.Geopoint;
+import com.microsoft.maps.MapElementLayer;
+import com.microsoft.maps.MapIcon;
+import com.microsoft.maps.MapImage;
+import com.microsoft.maps.MapView;
 import com.samar.location.models.House;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -53,6 +65,14 @@ public class ViewHouseDetailsActivity  extends AppCompatActivity {
     TextView housecardcity,contactPerson,houseNo,location,street,phone,price,size;
      List<String> imageUrls ;
     private static final int CALL_PERMISSION_REQUEST_CODE = 1;
+    private MapView mapView;
+    private FloatingActionButton mFab;
+    private MapElementLayer mPinLayer;
+    private MapImage mPinImage;
+    private int mUntitledPushpinCount = 0;
+    private Geopoint geopoint;
+    private static final String MY_API_KEY = "AlwLTKgevIemLkhFY8wA2oDQwpxY8SBBAR8a5dXymXDFKTmfGWKkXnJGQkGzXUMM";
+    private House house;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +88,7 @@ public class ViewHouseDetailsActivity  extends AppCompatActivity {
 
 
 
-        House house = (House) getIntent().getSerializableExtra("house");
+         house = (House) getIntent().getSerializableExtra("house");
 
 
 
@@ -142,6 +162,17 @@ public class ViewHouseDetailsActivity  extends AppCompatActivity {
         RecyclerViewHorizontalAdapter adapter = new RecyclerViewHorizontalAdapter(house.getImages(),housecardImage );
         recyclerView.setAdapter(adapter);
 
+
+        mapView = findViewById(R.id.mapView);
+        mapView.setCredentialsKey(MY_API_KEY);
+        mapView.onCreate(savedInstanceState);
+
+        mPinLayer = new MapElementLayer();
+        mapView.getLayers().add(mPinLayer);
+        mPinImage = getPinImage();
+        geopoint = new Geopoint(house.getLatitude(), house.getLongitude());
+        addPin(geopoint, house.getContactPerson()+"'s House");
+
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -155,6 +186,37 @@ public class ViewHouseDetailsActivity  extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Permission denied to make phone calls", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+    private MapImage getPinImage() {
+        // Create a pin image from a drawable resource
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_pin, null);
+
+        int width = drawable.getIntrinsicWidth();
+        int height = drawable.getIntrinsicHeight();
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return new MapImage(bitmap);
+    }
+
+    private void addPin(Geopoint location, String title) {
+        // Add a pin to the map at the given location
+        MapIcon pushpin = new MapIcon();
+        pushpin.setLocation(location);
+        pushpin.setTitle(title);
+        pushpin.setImage(mPinImage);
+
+        pushpin.setNormalizedAnchorPoint(new PointF(0.5f, 1f));
+        if (title.isEmpty()) {
+            pushpin.setContentDescription(String.format(
+                    Locale.ROOT,
+                    "Untitled pushpin %d",
+                    ++mUntitledPushpinCount));
+        }
+        mPinLayer.getElements().add(pushpin);
     }
 }
 
--- app/src/main/java/com/samar/location/homepage/RecyclerViewAdapter.java
+++ app/src/main/java/com/samar/location/homepage/RecyclerViewAdapter.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Filter;
@@ -216,6 +217,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         imageSlider.startSliding(5000); // with new period
         imageSlider.setImageList(imageList);
+
 
 
         holder.housecardCity.setText(house.getCity().toUpperCase()+", TUNISIA");
